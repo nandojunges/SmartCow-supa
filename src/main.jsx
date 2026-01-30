a881c39cdde1a95139ff2d6ca21ef44be8a31636
nandojunges <ferjunges@outlook.com>
2026-01-30 18:10:09 -0300

    Revert "Centralize auth and fazenda bootstrap in AuthContext"

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { FazendaProvider } from "./context/FazendaContext.jsx";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <FazendaProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        <App />
       </FazendaProvider>
     </BrowserRouter>
   </React.StrictMode>
