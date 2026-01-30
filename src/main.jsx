4eb6a1abab4c666a8f74b3cc8724fcbabd33dd50
nandojunges <ferjunges@outlook.com>
2026-01-30 18:00:42 -0300

    Centralize auth and fazenda bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { FazendaProvider } from "./context/FazendaContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <FazendaProvider>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </FazendaProvider>
     </BrowserRouter>
   </React.StrictMode>
