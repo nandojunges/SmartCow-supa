29db8c9aa91e6ab0e1557d102a30256f81e62ad4
nandojunges <ferjunges@outlook.com>
2026-02-01 17:56:37 -0300

    Delete src/Pages/Animais/FichaAnimal directory

--- src/Pages/Animais/FichaAnimal/FichaAnimalReproducao.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/Animais.jsx
-export default function Animais() {
-  return (
-    <h1 style={{ fontSize: 24, fontWeight: 600 }}>
-      Página ANIMAIS – em construção.
-    </h1>
-  );
-}
